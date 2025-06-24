d23d503f5eadd4d8dd2c003e29aaed1fe7c54be3
JoaumVictor <94501287+JoaumVictor@users.noreply.github.com>
2025-06-24 16:39:57 -0300

    feat: ajustar estilos e preços nas opções de produtos

--- src/components/productOptions/OptionGroupWrapper.tsx
+++ src/components/productOptions/OptionGroupWrapper.tsx
@@ -15,13 +15,13 @@ export default function OptionGroupWrapper({
 }: Props) {
   return (
     <section className="bg-white my-[2px] p-4 space-y-2 w-full">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between mb-4">
         <div>
           <h2 className="text-md font-bold text-neutral-900">{title}</h2>
           <p className="text-sm text-neutral-500">{description}</p>
         </div>
         {required && (
-          <span className="text-xs text-white bg-neutrals-700 bg-neutrals-200 rounded px-2 py-0.5">
+          <span className="text-xs text-white bg-neutrals-700 bg-neutrals-200 rounded px-[4px] py-[6px]">
             obrigatório
           </span>
         )}
--- src/components/productOptions/SingleOptionGroup.tsx
+++ src/components/productOptions/SingleOptionGroup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProductOption } from "@/types/store";
+import Icon from "../ui/Icon";
 
 interface Props {
   option: ProductOption;
@@ -17,21 +18,29 @@ export default function SingleOptionGroup({
       {option.items.map((item) => (
         <li
           key={item.id}
-          className="flex items-center justify-between border-b border-neutral-200 pb-2 cursor-pointer"
+          className="flex items-center justify-between  pb-2 cursor-pointer"
           onClick={() => onChange(option.id, item.id)}
         >
           <div className="flex items-center gap-2">
             <span
               className={`w-5 h-5 rounded-full border-2 ${
                 selected === item.id
-                  ? "border-purple-600 bg-purple-600"
+                  ? "border-purple-700 bg-purple-700"
                   : "border-neutral-300"
               }`}
             />
+            {item.originalPrice && <Icon name="money" width={24} height={24} />}
             <span className="text-sm text-neutral-900">{item.name}</span>
           </div>
 
-          {item.price > 0 && (
+          {item.originalPrice ? (
+            <p className="text-neutral-500 text-xs">
+              de {item.originalPrice.toFixed(2).replace(".", ",")} por
+              <span className="text-success text-sm font-bold ml-2">
+                +R$ {item.price.toFixed(2).replace(".", ",")}
+              </span>
+            </p>
+          ) : (
             <span className="text-purple-500 text-sm font-bold">
               +R$ {item.price.toFixed(2).replace(".", ",")}
             </span>
