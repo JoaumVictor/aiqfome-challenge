2e49219e8b2fcf92b19d95fb39ee28e8d695b82b
JoaumVictor <94501287+JoaumVictor@users.noreply.github.com>
2025-06-25 14:07:21 -0300

    feat: atualizar configuração do PostCSS para suporte a Turbopack

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+// postcss.config.js
+const isTurbopack =
+  process.env.NEXT_RUNTIME === "edge" || process.env.TURBOPACK === "1";
+
+module.exports = {
+  plugins: isTurbopack
+    ? {
+        "@tailwindcss/postcss": {},
+      }
+    : {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
 };
-export default config;
